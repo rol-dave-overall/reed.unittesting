46f1c3bb2278730496a54d79fb46d960d7c2a367
Dave Overall <dave.overall@reedonline.co.uk>
2017-06-26 14:36:25 +0100

    New comment

--- reed.unittesting.xmltransform/Validators/XmlTransformationLogger.cs
+++ reed.unittesting.xmltransform/Validators/XmlTransformationLogger.cs
@@ -26,6 +26,7 @@ namespace Reed.UnitTesting.Validators
         /// </summary>
         private readonly StringBuilder verboseMessageStore = new StringBuilder();
 
+        //god knows
         private const string IndentStringPiece = "  ";
 
         /// <summary>
